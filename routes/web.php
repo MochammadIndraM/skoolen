d9e4c1a386dfd8e9f8d2cdd27d2d59bd9c4df13a
MochammadIndraM <indrapagah43@gmail.com>
2024-06-03 20:27:09 +0700

    kategori hewan

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\countingFeatures\CountMenusController;
 use App\Http\Controllers\countingFeatures\LearnNumberController;
 use App\Http\Controllers\sainsFeatures\SainsMenuController;
+use App\Http\Controllers\sainsFeatures\HewanController;
+
 
 Route::get('/', function () {
     return view('page.landing-page');
@@ -13,3 +15,4 @@ Route::get('/belajar-angka', [CountMenusController::class, 'index'])->name('coun
 Route::get('/belajar-angka/learn-number', [LearnNumberController::class, 'index'])->name('countingFeatures.LearnNumber');
 
 Route::get('/sains', [SainsMenuController::class, 'index'])->name('sainsFeatures.SainsMenu');
+Route::get('/hewan', [HewanController::class, 'index'])->name('sainsFeatures.Hewan');

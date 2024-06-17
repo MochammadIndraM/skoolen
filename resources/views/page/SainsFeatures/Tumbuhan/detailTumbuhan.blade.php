9522dd283e0529b4a76b79ad3c434e6ccd6e666e
aryafathorozi <aryafathorozi@gmail.com>
2024-06-18 01:01:14 +0700

    update calling image in fitur User/cuaca, hewan, tumbuhan

--- resources/views/page/SainsFeatures/Tumbuhan/detailTumbuhan.blade.php
+++ resources/views/page/SainsFeatures/Tumbuhan/detailTumbuhan.blade.php
@@ -6,8 +6,13 @@
         <div class="w-full max-w-[800px] px-8 py-8">
             <div class="bg-[#FF5580] w-full rounded-3xl flex flex-col items-center">
                 <div class="w-full overflow-hidden rounded-t-3xl">
-                    <img src="/assets/img/sains/tumbuhan/{{ $tumbuhan->gambar }}"
-                        class="w-full h-[300px] md:h-[400px] lg:h-[500px] object-cover" alt="{{ $tumbuhan->nama_tumbuhan }}">
+                    @if (file_exists(public_path('assets/img/sains/tumbuhan/' . $tumbuhan->gambar)))
+                        <img class="w-full h-[300px] md:h-[400px] lg:h-[500px] object-cover"
+                            src="{{ asset('assets/img/sains/tumbuhan/' . $tumbuhan->gambar) }}" alt="{{ $tumbuhan->gambar }}">
+                    @else
+                        <img class="w-full h-[300px] md:h-[400px] lg:h-[500px] object-cover"
+                            src="{{ asset('/storage/' . $tumbuhan->gambar) }}" alt="">
+                    @endif
                 </div>
                 <div class="p-8">
                     <p class="text-[#FFFF80] text-[24px] md:text-[34px] lg:text-[44px]">{{ $tumbuhan->nama_tumbuhan }}</p>
